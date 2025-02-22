201cd0ab6d290d912caa35973867ea77bd57a10b
lnsohu <35254544+lnsohu@users.noreply.github.com>
2025-02-22 22:25:26 +0800

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const root = document.getElementById('root');
+root.innerHTML = `
+  <div style="text-align: center; margin-top: 50px;">
+    <h1>Hello World!</h1>
+    <p>This is a simple app deployed on Netlify.</p>
+  </div>
+`;
