f7f356dd127287997303da476def613b9bc9f3bb
lnsohu <35254544+lnsohu@users.noreply.github.com>
2025-02-23 21:57:43 +0800

    db.js: 添加debug信息

--- netlify/functions/db.js
+++ netlify/functions/db.js
@@ -4,7 +4,24 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.supabase_db_SUPABASE_URL; // 使用 API URL
 const supabaseKey = process.env.supabase_db_NEXT_PUBLIC_SUPABASE_ANON_KEY; // 使用 anon key
 
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Supabase environment variables are missing');
+}
+
 // 初始化 Supabase 客户端
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-export default supabase;
+// export default supabase; // without debugging
+export async function getTravels() {
+  const { data, error } = await supabase
+    .from('travels')
+    .select('*');
+
+  if (error) {
+    console.error('Debug db.js: Error fetching travels:', error); // 添加日志
+    throw new Error('Debug db.js throw error: Failed to get data from DB travels');
+  }
+
+  return data;
+}
+
